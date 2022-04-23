69f8e84c516af8c4f6113d6f0ec369a06d77fe01
Daniel's <danieluk79@yahoo.co.uk>
2022-04-23 11:56:55 +0100

    Doing final  work

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println( "Hello word");
-
+        System.out.println("B25 learning Git and GitHub");
     }
 }
